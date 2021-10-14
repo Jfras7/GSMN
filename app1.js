ca76fbbf2697b8623c73281a40e0e14f82a2fc76
Josh Frasure <joshfrasure2@gmail.com>
2021-10-13 22:16:14 -0400

    added javascript file

---
+++ app1.js
@@ -0,0 +1,8 @@
+console.log('GSMN')
+
+let userName = prompt('What is your Name?')
+console.log(userName)
+let userAge = prompt('How old are you?')
+console.log(userAge)
+
+document.write('<h2>Welcome ' + userName + '</h2>')
