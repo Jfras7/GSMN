5aa7799a2ef23bfeecc2f2561bf1a6949417f710
Josh Frasure <joshfrasure2@gmail.com>
2021-10-14 23:04:08 -0400

    updated

--- app1.js
+++ app1.js
@@ -1,8 +1,9 @@
 console.log('GSMN')
 
 let userName = prompt('What is your Name?')
-console.log(userName)
+    console.log(userName)
+
 let userAge = prompt('How old are you?')
-console.log(userAge)
+    console.log(userAge)
 
 document.write('<h2>Welcome ' + userName + '</h2>')
