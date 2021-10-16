e9c177c0ddd1c1614f1d7a82123d2ee8745787d4
Josh Frasure <joshfrasure2@gmail.com>
2021-10-15 23:44:34 -0400

    added loops and img for lab 08

--- app1.js
+++ app1.js
@@ -19,3 +19,42 @@ let userAnswer = prompt('Do you want to join the Champions Club?')
 if (userAnswer.toLowerCase() == 'yes'){
     document.write('APPLY')
 }
+
+function randomNumber(){
+
+    let randNum = Math.floor(Math.random() * 100) + 1;
+    return randNum
+}
+
+function guessingGame(){
+    correctAnswer = randomNumber();
+    console.log(correctAnswer);
+    let guesses = 10;
+    for(let i = 0; i < guesses; i++){
+        let guessLeft = guesses - i
+        let userGuess = parseInt(prompt('Enter a Number 1-100. You have ' + guessLeft + ' guesses left!'));
+
+        while(userGuess < 1 || userGuess > 100){
+            userGuess = parseInt(prompt('WRONG!Try Again 1-100'));
+        }
+        if(userGuess == correctAnswer){
+            alert('You Actually Did It!');
+            break;
+        } else if(userGuess > correctAnswer){
+            alert('You are to high!');
+        } else if(userGuess < correctAnswer){
+            alert('You are to low!');
+        }
+        else{
+            alert('Follow Directions!');
+        }
+    }
+}
+guessingGame();
+
+function picturesOnPage(){
+    let numberOfPictures = prompt('How much VSM do you want?')
+    for(let i = 0; i < numberOfPictures; i++){
+        document.write('<img src="https://pbs.twimg.com/profile_images/1443937530043273224/aZWclSE3_400x400.jpg">')
+    }
+}
